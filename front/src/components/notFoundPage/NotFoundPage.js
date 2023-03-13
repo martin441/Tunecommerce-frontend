c5571942e6ebedaa07b13a1720dc6a2893f913b7
Martin Machado <martinmachado441@gmail.com>
2023-03-13 13:02:35 -0300

    correccion errores package.json y npm i

--- front/src/components/notFoundPage/NotFoundPage.js
+++ front/src/components/notFoundPage/NotFoundPage.js
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
 
 const NotFoundPage = () => {
   return (
     <div>
       <h1>Error 404: Página no encontrada</h1>
       <p>Lo siento, la página que estás buscando no existe.</p>
-      <Button variant="contained" color="primary" component={Link} to="/">
+      <button variant="contained" color="primary" component={Link} to="/">
         Volver a la página principal
-      </Button>
+      </button>
     </div>
   );
 };
